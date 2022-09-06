55041bdce23d1f08a7c2e15a661fc426dfaadcf6
Wemerson Nino <wemersonnino@gmail.com>
2022-09-06 17:21:47 -0300

    delete widget sidebar

--- wp-content/themes/meutudoblog/widgets.php
+++
@@ -1,8 +0,0 @@
-<?php
-function meutudoblog_widgets(){
-    register_sidebar([
-        'meutudoblog_sidebar'  => __('Theme Sidebar','meutudoblog'),
-        'id'                    => 'meutudoblog_sidebar',
-        'description'           => __('Sidebar for the blog meutudo', 'meutudoblog')
-    ]);
-}
