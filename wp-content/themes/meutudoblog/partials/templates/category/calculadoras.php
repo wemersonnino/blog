2849c862b8b0ac124fe92a7e62f5adbc0ddcb1f2
Wemerson Nino <wemersonnino@gmail.com>
2022-09-19 17:24:54 -0300

    Correção chamada de posts do type pagina para exibição das paginas de calculadoras

--- wp-content/themes/meutudoblog/partials/templates/category/calculadoras.php
+++ wp-content/themes/meutudoblog/partials/templates/category/calculadoras.php
@@ -1,14 +1,12 @@
-<?php $categorias = implode(', ', array_map(function($category) {
-    return '<a href="' . esc_url(home_url('/categoria/' . $category->slug . '/')) . '" class="categoria">' . $category->name . '</a>';
-}, get_the_terms(get_the_ID(), 'category'))); ?>
+
 <?php
 
 $posts = new WP_Query(array(
-    'post_type' => 'post',
+    'post_type' => ['post','page'],
     'orderby' => 'date',
     'order' => 'DESC',
     'posts_per_page' => 3,
-    'category_name' => 'calculadoras',
+    'post__in' => [36700]
 ));
 
 ?>
@@ -27,7 +25,7 @@ $posts = new WP_Query(array(
                 <section id="post" class="posts">
                     <div class="row align-content-start">
                         <?php while($posts->have_posts()) : $posts->the_post(); ?>
-                        <?php $categorias = array_map(function($object) { return $object->name; }, get_the_terms(get_the_ID(), 'category')); ?>
+<!--                        --><?php //$categorias = array_map(function($object) { return $object->name; }, get_the_terms(get_the_ID(), 'category')); ?>
                         <div class="col-12 col-sm-6 col-lg-4">
                            <?php get_template_part('partials/items/post', 'card'); ?>
                         </div><!--\Box Post Calc-->
