171a258431b69bb3931543dc0fd0d921d97e76ab
Wemerson Nino <wemersonnino@gmail.com>
2022-09-19 12:33:47 -0300

    Foi criado template category-calculadoras para que quando chamado página de category calculadora ser exibido diferente de como as categorias normais do blog são exibidas. category-calculadoras->templates>category>calculadora(template)

--- wp-content/themes/meutudoblog/partials/templates/page/conteudo.php
+++ wp-content/themes/meutudoblog/partials/templates/page/conteudo.php
@@ -1,6 +1,7 @@
 <section class="page-conteudo">
   <div class="container">
-    <?php if (get_field('page-mostrar-titulo')) : ?><h1 class="titulo"><?php the_title(); ?></h1><?php endif; ?>
+    <?php if (get_field('page-mostrar-titulo')) : ?>
+        <h1 class="titulo"><?php the_title(); ?></h1><?php endif; ?>
 
     <div class="conteudo-wysiwyg">
       <?php the_content(); ?>
