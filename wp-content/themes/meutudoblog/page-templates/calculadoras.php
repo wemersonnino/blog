dbc79d792be6a1fc471f66d86dd3b006bd590aae
Wemerson Nino <wemersonnino@gmail.com>
2022-09-20 09:51:10 -0300

    Criado template para a pagina home de calculadoras, ela servirá para mapear o breadcrumb

--- wp-content/themes/meutudoblog/page-templates/calculadoras.php
+++ wp-content/themes/meutudoblog/page-templates/calculadoras.php
@@ -1,27 +1,24 @@
 <?php
 /*
- Template Name: calculadoras-front
+ Template Name: calculadora home
  */
 ?>
 <?php get_header(); ?>
-<?php get_template_part('/partials/topos/padrao'); ?><!--//header-->
-<main id="main-container">
-    <section class="container">
-        <article class="row justify-content-start">
-            <div class="w-100"></div><br><br>
-            <section id="content-post" class="post-conteudo col-md-8 col-lg-8 col-xl-8">
-                <div class="row align-items-center">
-                    <h1 class="title-nome nome">
-                        <?php echo the_title()?>
-                    </h1><!--\title-->
-                    <div class="w-100"></div><br><br>
-<?php get_template_part('/partials/templates/page/conteudo-calculadoras'); ?><!--//main-->
 
-<?php if (get_field('/page-newsletter-habilitado')) get_template_part('partials/blocos/home-calculadoras'); ?>
+<?php get_template_part('partials/topos/padrao'); ?>
 
-<?php if (get_field('/page-baixe-o-aplicativo-habilitado'))get_template_part('partials/blocos/baixe-o-aplicativo'); ?>
+<?php get_template_part('partials/templates/category/calculadoras') ?>
 
-<?php get_template_part('/partials/rodapes/padrao'); ?>
+<?php get_template_part('partials/templates/post/comentarios'); ?>
+
+<?php get_template_part('partials/blocos/newsletter'); ?>
+
+<?php //get_template_part('partials/blocos/postagens-sugeridas'); ?>
+
+<?php get_template_part('partials/blocos/baixe-o-aplicativo'); ?>
+
+<?php get_template_part('partials/rodapes/padrao'); ?>
+
+<?php get_template_part('partials/blocos/compartilhar-lateral'); ?>
 
 <?php get_footer(); ?>
-
