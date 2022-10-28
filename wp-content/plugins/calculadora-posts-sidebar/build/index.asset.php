308f229efcd18533427926b8a2ab83a76a25862e
Wemerson Pereira <wemersonnino@gmail.com>
2022-10-28 16:36:02 -0300

    feat-[Onboarding #feature-32x88wg-simuladores] - Ajustes layout com o figma

--- wp-content/plugins/calculadora-posts-sidebar/build/index.asset.php
+++ wp-content/plugins/calculadora-posts-sidebar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '19ab5bf146d7887a810e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e4a8bc5f3ea120f27e29');
