dc05d5c7845b24aad775b56abaf3e879580ba458
Wemerson Pereira <wemersonnino@gmail.com>
2022-10-28 15:10:06 -0300

    feat-[Onboarding #feature-32x88wg-simuladores] - Sidebar style maps and backend

--- wp-content/plugins/calculadora-posts-sidebar/build/index.asset.php
+++ wp-content/plugins/calculadora-posts-sidebar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '966060ab9c9db304c0a2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '19ab5bf146d7887a810e');
