7d2dd9e5343c09e8a9811608d44f5b6b5528062f
Wemerson Pereira <wemersonnino@gmail.com>
2022-10-31 15:13:45 -0300

    feat-[Onboarding #feature-32x88wg-simuladores] - Ajustes layout simuladores

--- wp-content/plugins/calculadora-simulador/build/index.asset.php
+++ wp-content/plugins/calculadora-simulador/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4223d4fc6de3428db573');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6ca00570804b92c991bb');
