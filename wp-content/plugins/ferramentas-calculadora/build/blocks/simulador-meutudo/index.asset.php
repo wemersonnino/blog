dfc7cf08f34f533235b7741fd41adf66bbb7d466
Wemerson Nino <wemersonnino@gmail.com>
2022-12-07 19:05:28 -0300

    criação do template para o block simulador

--- wp-content/plugins/ferramentas-calculadora/build/blocks/simulador-meutudo/index.asset.php
+++ wp-content/plugins/ferramentas-calculadora/build/blocks/simulador-meutudo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '49345f769c1ef700f608');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ee809f194c3c6b684612');
