e80913bca31c574e5bb3251c0937de7f813b69cd
Wemerson Nino <wemersonnino@gmail.com>
2022-12-15 10:41:56 -0300

    fix-3d3d6jv - build files para prod

--- wp-content/plugins/ferramentas-meutudo-calculadora/build/blocks/simulators/index.asset.php
+++ wp-content/plugins/ferramentas-meutudo-calculadora/build/blocks/simulators/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '99454f4fd352d0b191ea');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '62da37762bbc4ac23926');
